eefa04d9626f1428c6e12786efe13c178de46fc6
ферденант никита <klin.fray@mail.ru>
2020-10-14 16:46:20 +0300

    -

--- laba_5/Страницы/Page2.xaml.cs
+++ laba_5/Страницы/Page2.xaml.cs
@@ -38,45 +38,40 @@ namespace laba_5.Страницы
             
         }
 
-
-      
-
-
-
         private void Textbox_Last_Name_TextChanged(object sender, TextChangedEventArgs e)
         {
-            Filter();
+            //Filter();
             //1 часть работы 
-            //var list = context.User.ToList();
-            //  listview_form.ItemsSource = list.Where(i => i.LastName.ToUpper().Contains(Textbox_Last_Name.Text.ToUpper()) ) ;
+            var list = context.User.ToList();
+            listview_form.ItemsSource = list.Where(i => i.LastName.ToUpper().Contains(Textbox_Last_Name.Text.ToUpper()));
 
 
         }
 
         private void Textbox_First_Name_TextChanged(object sender, TextChangedEventArgs e)
         {
-            Filter();
+            //Filter();
             //1 часть работы 
-            // var list = context.User.ToList();
+            var list = context.User.ToList();
             //Ниже код с ошибкой
-            // listview_form.ItemsSource = list.Where(i => i.MiddleName.ToUpper().Contains(Textbox_First_Name.Text.ToUpper()));
+             listview_form.ItemsSource = list.Where(i => i.MidleName.Contains(Textbox_First_Name.Text) || i.FirstName.Contains(Textbox_First_Name.Text));
             //Перевод в верхний регистр 
 
         }
         //i.MiddleName.Contains(Textbox_Last_name.Text.ToUpper()
         private void Textbox_Login_TextChanged(object sender, TextChangedEventArgs e)
         {
-            Filter();
+            //Filter();
 
             //1 часть работы 
-            //var list = context.User.ToList();
-            //listview_form.ItemsSource = list.Where(i => i.Login.ToLower().Contains(Textbox_Login.Text));
+            var list = context.User.ToList();
+            listview_form.ItemsSource = list.Where(i => i.Login.ToLower().Contains(Textbox_Login.Text));
 
         }//Перевод в верхний регистр 
 
         private void Combobox_Role_SelectionChange(object sender, SelectionChangedEventArgs e)
         {
-            Filter();
+            //Filter();
             //1 часть работы 
             var list = context.User.ToList();
             if (Combobox_Role.SelectedIndex == 0)
@@ -92,20 +87,20 @@ namespace laba_5.Страницы
         }
         public void Filter()
         {
-            var list = context.User.Where(i => i.LastName.Contains(Textbox_Last_name.Text))
-                                   .Where(i => i.MidleName.Contains(Textbox_First_Name.Text) || i.FirstName.Contains(Textbox_First_Name.Text))
-                                   .Where(i => i.Login.Contains(Textbox_Login.Text)).ToList();
-            listview_form.ItemsSource = list;
-            if (Combobox_Role.SelectedIndex == 0)
-            {
-                listview_form.ItemsSource = list;
-            }
-            else
-            {
-                var Role = Combobox_Role.SelectedItem as Role;
-                list = list.Where(i => i.idRole == Role.idRole).ToList();
-                listview_form.ItemsSource = list;
-            }
+            //var list = context.User.Where(i => i.LastName.Contains(Textbox_Last_name.Text))
+            //                       .Where(i => i.MidleName.Contains(Textbox_First_Name.Text) || i.FirstName.Contains(Textbox_First_Name.Text))
+            //                       .Where(i => i.Login.Contains(Textbox_Login.Text)).ToList();
+            //listview_form.ItemsSource = list;
+            //if (Combobox_Role.SelectedIndex == 0)
+            //{
+            //    listview_form.ItemsSource = list;
+            //}
+            //else
+            //{
+            //    var Role = Combobox_Role.SelectedItem as Role;
+            //    list = list.Where(i => i.idRole == Role.idRole).ToList();
+            //    listview_form.ItemsSource = list;
+            //}
         }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
